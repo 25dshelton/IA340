aaaac14be03738aafab6d7fe8f46fc69aae02bed
25dshelton <143727483+25dshelton@users.noreply.github.com>
2023-09-13 17:34:36 -0400

    Update Lab3.sql

--- Lab3.sql
+++ Lab3.sql
@@ -21,3 +21,10 @@ values ('s1@jmu.edu','c1'),
 	   ('s4@jmu.edu','c2'),
 	   ('s2@jmu.edu','c3'),
 	   ('s3@jmu.edu','c3')
+q2.5
+insert into professor(p_email,p_name,office)
+values ('p3@jmu.edu','p3','o3')
+
+insert into course(c_number,c_name,room,p_email)
+values ('c4','facebook','r1','p3@jmu.edu')
+--have to change professor table first because you're adding a new email (person) assigned to a course
